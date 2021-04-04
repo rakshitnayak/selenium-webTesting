f1f82eba02d2e535c3c66746b5227d541f38d7a7
rakshit <rdsonnn@gmail.com>
2021-04-04 23:17:51 +0530

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Testing(Automation)
+
+This Repositry includes selenium tests for a web page ,as a part of my GRIP(graduation Rotational internship program) @ The sparks foundation.
---
+++ Tests/sampleTest.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time  
+from selenium.webdriver.common.keys import Keys  
+
+print("sample test case started")  
+Path="E:\RAKSHIT FILES\WEB DEVLOPMENT\INternship works\selenium-tests\Browsers\chromedriver.exe"
+driver=webdriver.Chrome(Path)
+driver.maximize_window()  
+
+driver.get("https://www.google.com/")  
+
+driver.find_element_by_name("q").send_keys("covid19")  
+time.sleep(3)  
+driver.find_element_by_name("btnK").send_keys(Keys.ENTER)  
+time.sleep(3)  
+print("sample test case successfully completed") 
